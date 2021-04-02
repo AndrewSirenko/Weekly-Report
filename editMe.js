8fe7f8815f46aeccc374ecfe38dbb864ebd66771
Andrew Sirenko <68304519+AndrewSirenko@users.noreply.github.com>
2021-04-02 14:57:00 -0400

    Updating Spec IDs

--- editMe.js
+++ editMe.js
@@ -1,6 +1,6 @@
 // Your site's Google Analytics VIEW ID
-const VIEW_ID = 'YOUR_VIEW_ID';
+const VIEW_ID = '7024503';
 // FolderId of folder which stores weekly reports
-const FOLDER_ID = 'YOUR_DRIVE_FOLDER_ID';
+const FOLDER_ID = '1z8V9bFSBL94TlG-qY6yb_DUHqbwycSwK';
 // Doc ID of template document
-const TEMPLATE_DOC_ID = 'YOUR_TEMPLATE_DOC_ID';
+const TEMPLATE_DOC_ID = '13z0nQfjAa-DzD4Rq1WDOxoOTZ4GXXjB2oYWWQsPgPvY';
